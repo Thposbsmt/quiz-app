2c16225ca0f1794b893398cc85d1fc97b313046b
Thposbsmt <thposbsmt@protonmail.com>
2021-06-30 04:11:08 +0300

    fix quit system, add forms for rendering, add result for test

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,5 +1,10 @@
 from django.forms import ModelForm
+from django import forms
 
+BOOLEAN = [
+    ("True",'True'),
+    ("False","False"),
+]
 
 from .models import Quiz
 
@@ -7,3 +12,6 @@ class QuizForm(ModelForm):
     class Meta:
         model = Quiz
         fields = ('question', 'correct_answer')
+        
+class BooleanForm(forms.Form):
+    choice = forms.ChoiceField(label='',choices=BOOLEAN, widget=forms.RadioSelect)
--- quiz/views.py
+++ quiz/views.py
@@ -4,7 +4,7 @@ from django.views.generic.edit import CreateView
 import random
 
 from .models import Quiz
-from .forms import QuizForm
+from .forms import QuizForm, BooleanForm    
 
 def index(request):
     template = loader.get_template('quiz/index.html')
@@ -12,14 +12,37 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def test(request):
-    template = loader.get_template('quiz/test.html')
+    if request.method == 'POST':
+        form1 = BooleanForm(request.POST, prefix="1")
+        form2 = BooleanForm(request.POST, prefix="2")
+        form3 = BooleanForm(request.POST, prefix="3")
+        
+        answer1 = request.POST['hidden_answer0']
+        answer2 = request.POST['hidden_answer1']
+        answer3 = request.POST['hidden_answer2']
+        print(answer1)
+        print(answer2)
+        print(answer3)
+        counter = 0
+        
+        if form1.is_valid() and form2.is_valid() and form3.is_valid():
+            if form1.cleaned_data['choice'] == answer1:
+                counter += 1
+            if form2.cleaned_data['choice'] == answer2:
+                counter += 1
+            if form3.cleaned_data['choice'] == answer3:
+                counter += 1
+            return render(request, "quiz/result.html", {"counter":counter})
+    else:
+        items = list(Quiz.objects.all())
+        random_items = random.sample(items, 3)
+        form1 = BooleanForm(prefix="1")
+        form2 = BooleanForm(prefix="2")
+        form3 = BooleanForm(prefix="3")
+        context = {'quizes':random_items, 'form1':form1, 'form2':form2, 'form3':form3}
+        
+        return render(request, "quiz/test.html", context)
     
-    items = list(Quiz.objects.all())
-    random_items = random.sample(items, 3)
-    
-    context = {'quizes':random_items}
-    
-    return HttpResponse(template.render(context, request))
 
 class QuizCreateView(CreateView):
     template_name = 'quiz/create.html'
